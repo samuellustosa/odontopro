44b89ec5fe0d474bb5710048ecebb699ed7ce165
unknown <grupomateus@PISM211-D0001.mateus.dc>
2025-09-13 15:00:26 -0300

    CRIANDO GRID DE HORÁRIOS DA EMPRESA

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -1,6 +1,15 @@
 "use client"
 
 import { useRouter, useSearchParams } from 'next/navigation'
+import {} from '@/components/ui/scroll-area'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription
+} from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 interface AppointmentsListProps {
   times: string[]
@@ -15,8 +24,31 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
 
 
   return (
-    <div>
-      LISTAGEM DE HORARIOS
-    </div>
+    <Card>
+      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+        <CardTitle className='text-xl md:text-2x1 font-bold'>
+            Agendamentos
+        </CardTitle>
+
+        <button>SELECIONAR DATA</button>  
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className='h-[calc(100vh-20rem)] lg:h[calc(100vh-15rem] pr-4'>
+            {times.map((slot) => {
+              return(
+                <div key={slot}
+                className='flex items-center py-2 border-t last:border-b'
+                >
+                  <div className='w-16 text-sm font-semibold'>{slot}</div>
+                  <div className='flex-1 text-sm text-gray-500'>
+                    Disponível
+                  </div>
+                </div>
+              )
+            })}
+        </ScrollArea>
+      </CardContent>
+    </Card>
   )
 }
