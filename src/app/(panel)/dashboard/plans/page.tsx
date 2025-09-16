e5b8dc70c07746185a26a13c0fd5cee2cc90682b
unknown <grupomateus@PISM211-D0001.mateus.dc>
2025-09-15 21:43:27 -0300

    configurando stripe

--- src/app/(panel)/dashboard/plans/page.tsx
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -1,6 +1,7 @@
 import getSession from '@/lib/getSession'
 import { redirect } from 'next/navigation'
 import { GridPlans } from './_components/grid-plans'
+import { getSubscription } from '@/utils/get-subscription'
 
 export default async function Plans() {
   const session = await getSession()
@@ -9,10 +10,18 @@ export default async function Plans() {
     redirect("/")
   }
 
+  const subscritpion = await getSubscription({ userId: session?.user?.id! })
+
   return (
     <div>
 
-      <GridPlans />
+      {subscritpion?.status !== "active" && (
+        <GridPlans />
+      )}
+
+      {subscritpion?.status === "active" && (
+        <h1>Você tem uma assinatura ativa</h1>
+      )}
 
     </div>
   )
